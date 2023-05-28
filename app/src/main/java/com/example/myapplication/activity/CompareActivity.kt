292f7d64b4a7b87f49d4fb4845029e78f2ace4a1
Yeo Seung Jun <kongsa0419@gmail.com>
2023-05-29 00:34:15 +0900

    ML kit 그려지긴 하네 - 보완 필요

--- app/src/main/java/com/example/myapplication/activity/CompareActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/CompareActivity.kt
@@ -2,22 +2,21 @@ package com.example.myapplication.activity
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.BitmapFactory
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.util.Base64
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.R
 import com.example.myapplication.activity.BaseActivity.Companion.LOG_TAG
 import com.example.myapplication.databinding.ActivityCompareBinding
-import com.example.myapplication.util.ImageUtil
 import com.example.myapplication.util.SharedPreferencesUtil
 import com.example.myapplication.vision.FaceContourGraphic
 import com.example.myapplication.vision.GraphicOverlay
-
+import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
@@ -38,8 +37,8 @@ class CompareActivity : AppCompatActivity(){
     private lateinit var trnsImgUri : Uri
     private lateinit var newImgUri : Uri
 
-    var mGraphicOverlay: GraphicOverlay? = null
-
+    var trnsGraphicOverlay: GraphicOverlay? = null
+    var newGraphicOverlay: GraphicOverlay? = null
 
     // High-accuracy landmark detection and face classification
     val highAccuracyOpts = FaceDetectorOptions.Builder()
@@ -53,51 +52,85 @@ class CompareActivity : AppCompatActivity(){
     // val detector = FaceDetection.getClient();
     val detector = FaceDetection.getClient(highAccuracyOpts)
 
+
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityCompareBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        mGraphicOverlay = findViewById(R.id.graphic_overlay)
+        trnsGraphicOverlay = findViewById(R.id.trns_graphic_overlay)
+        newGraphicOverlay = findViewById(R.id.new_graphic_overlay)
 
-        val trnsBase64 = SharedPreferencesUtil.getString(getString(R.string.trns_pic)).toString()
-        val imageBytes = Base64.decode(trnsBase64, Base64.DEFAULT)
-        val trnsBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+        /**실전용*/
+//        val trnsBase64 = SharedPreferencesUtil.getString(getString(R.string.trns_pic)).toString()
+//        val imageBytes = Base64.decode(trnsBase64, Base64.DEFAULT)
+//        val trnsBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+//        val newStr = SharedPreferencesUtil.getString(getString(R.string.new_pic)).toString()
+//        trnsImgUri = ImageUtil.getImageUri(this@CompareActivity, trnsBitmap)
+//        newImgUri = Uri.parse(newStr)
 
-        val newStr = SharedPreferencesUtil.getString(getString(R.string.new_pic)).toString()
-
-        trnsImgUri = ImageUtil.getImageUri(this@CompareActivity, trnsBitmap)
-        newImgUri = Uri.parse(newStr)
+        //테스트용
+        trnsImgUri = Uri.parse("content://media/external/images/media/1000005770") // 입벌리고 미소 지은거
+        newImgUri =  Uri.parse("content://media/external/images/media/1000005771") // 찌뿌등(화낸거 같음)
 
         Log.d("URIURIURI", trnsImgUri.toString())
         Log.d("URIURIURI", newImgUri.toString())
 
-        //1
-        viewBinding.cmpTrnsPicImg.setImageBitmap(trnsBitmap)
+
+        viewBinding.cmpTrnsPicImg.setImageURI(trnsImgUri)
         viewBinding.cmpNewPicImg.setImageURI(newImgUri)
+
 
         val image_new: InputImage
         val image_trns: InputImage
-        try {
+        try{
             image_new = InputImage.fromFilePath(this@CompareActivity, newImgUri)
             image_trns = InputImage.fromFilePath(this@CompareActivity, trnsImgUri)
 
             if(image_new == null || image_trns == null) throw java.lang.NullPointerException()
 
-            processInputImage(image_new)
-            processInputImage(image_trns)
-
-        } catch (e: IOException) {
+            processInputImage(image_new, newGraphicOverlay!!)
+            processInputImage(image_trns,trnsGraphicOverlay!!)
+        }catch (e:Exception){
             e.printStackTrace()
         }
 
+
+//음....
+//        val trnsBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, trnsImgUri)
+//        val newBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, newImgUri)
+//
+//        runFaceContourDetection (trnsBitmap, trnsGraphicOverlay!!)
+//        runFaceContourDetection (newBitmap, newGraphicOverlay!!)
+
+
+        //1 TODO 이미지 셋에 Contour그린 사진으로 대체해줘야 함
+//        viewBinding.cmpTrnsPicImg.setImageBitmap(trnsBitmap)
+//        viewBinding.cmpTrnsPicImg.setImageURI(trnsImgUri)
+//        viewBinding.cmpNewPicImg.setImageURI(newImgUri)
+//
+//        val image_new: InputImage
+//        val image_trns: InputImage
+//        try {
+//            image_new = InputImage.fromFilePath(this@CompareActivity, newImgUri)
+//            image_trns = InputImage.fromFilePath(this@CompareActivity, trnsImgUri)
+//
+//            if(image_new == null || image_trns == null) throw java.lang.NullPointerException()
+//
+//            processInputImage(image_new)
+//            processInputImage(image_trns)
+//
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+
         //2 TODO 표정유사도 로직 구현
         //  2-1. 특징점 추출 , 벡터값 구하기, 값 도출,
-        CoroutineScope(Dispatchers.IO).launch{
-
-
-        }
+//        CoroutineScope(Dispatchers.IO).launch{
+//
+//
+//        }
 
 
 
@@ -132,17 +165,14 @@ class CompareActivity : AppCompatActivity(){
 
         val myListener = View.OnClickListener {
             when{
-                (it is Button)->{
+                (it is View)->{
                     Log.d("BTN_BTN_", it.id.toString())
 
                     if(it.id==R.id.cmp_goback){ //이전 액티비티로 이동 -> 다시 사진찍는 것임
-                        //TODO 테스트
                         finish() //테스트 요청
                     }
                     else if(it.id == R.id.cmp_gonext){ //표정 연습 다시
-                        //TODO 테스트
                         finish() //테스트 요청
-//                        facialPracAgain()
                     }else if(it.id == R.id.cmp_app_reset){ // 앱 재시작
                         restartApp() //테스트요청
                     }
@@ -150,8 +180,6 @@ class CompareActivity : AppCompatActivity(){
 
             }
         }
-
-
         val corr = false
         updateUI(corr)  //정답 여부에 따른 UI 업데이트
 
@@ -161,27 +189,30 @@ class CompareActivity : AppCompatActivity(){
         viewBinding.cmpGonext.setOnClickListener(myListener)
 
         viewBinding.cmpAppReset.setOnClickListener(myListener)
+
+        viewBinding.cmpUpperTv.setOnClickListener(myListener) //TESTING
     }
 
-    private fun processInputImage(image: InputImage) {
+
+    private fun processInputImage(image: InputImage, graphicOverlay: GraphicOverlay) {
         val result_new = detector.process(image)
-            .addOnSuccessListener { faces -> processFaceContourDetectionResult(faces)} //그림 그려주기?
+            .addOnSuccessListener { faces -> processFaceContourDetectionResult(faces, graphicOverlay)} //그림 그려주기?
             .addOnFailureListener { e ->e.printStackTrace() }
     }
 
 
-    private fun processFaceContourDetectionResult(faces: List<Face>) {
+    private fun processFaceContourDetectionResult(faces: List<Face>, graphicOverlay:GraphicOverlay) {
         // Task completed successfully
         if (faces.size == 0) {
             Log.d(LOG_TAG, "MLKIT_FACE_CONTOUR_ERROR")
             return
         }
-        mGraphicOverlay!!.clear()
+        graphicOverlay!!.clear()
         //간단하게 표시
         for (i in faces.indices) {
             val face: Face = faces[i]
-            val faceGraphic = FaceContourGraphic(mGraphicOverlay)
-            mGraphicOverlay!!.add(faceGraphic)
+            val faceGraphic = FaceContourGraphic(graphicOverlay)
+            graphicOverlay!!.add(faceGraphic)
             faceGraphic.updateFace(face)
             Log.d("ML_KIT", face.toString())
         }
@@ -190,12 +221,6 @@ class CompareActivity : AppCompatActivity(){
 //
 //        }
     }
-
-
-
-
-
-
 
 
 
